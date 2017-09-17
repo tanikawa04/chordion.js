da67980af70aeb2fce7e9448c827d0337a666fb9
tanikawa04 <tanikawa04@gmail.com>
2017-09-18 03:44:04 +0900

    refactor

--- src/chord-type.js
+++ src/chord-type.js
@@ -1,12 +1,7 @@
 import Interval from './interval';
 
-const _symbol = Symbol();
-
-export default class ChordType {
-  constructor(symbol, intervals, name) {
-    if (symbol !== _symbol) {
-      throw new Error('Cannot call constructor of ChordType.');
-    }
+class ChordType {
+  constructor(intervals, name) {
     this._intervals = intervals;
     this._name = name;
   }
@@ -22,76 +17,61 @@ export default class ChordType {
   equals(target) {
     return this.name === target.name;
   }
-
-  static get MAJOR() {
-    return new ChordType(_symbol, [Interval.P1, Interval.M3, Interval.P5], 'M');
-  }
-
-  static get SUSPENDED_FOURTH() {
-    return new ChordType(_symbol, [Interval.P1, Interval.P4, Interval.P5], 'sus4');
-  }
-
-  static get AUGMENT() {
-    return new ChordType(_symbol, [Interval.P1, Interval.M3, Interval.A5], 'aug');
-  }
-
-  static get MINOR() {
-    return new ChordType(_symbol, [Interval.P1, Interval.m3, Interval.P5], 'm');
-  }
-
-  static get DIMINISH() {
-    return new ChordType(_symbol, [Interval.P1, Interval.m3, Interval.d5], 'dim');
-  }
-
-  static get MAJOR_SEVENTH() {
-    return new ChordType(_symbol, [Interval.P1, Interval.M3, Interval.P5, Interval.M7], 'M7');
-  }
-
-  static get SEVENTH() {
-    return new ChordType(_symbol, [Interval.P1, Interval.M3, Interval.P5, Interval.m7], '7');
-  }
-
-  static get SEVENTH_SUSPENDED_FOURTH() {
-    return new ChordType(_symbol, [Interval.P1, Interval.P4, Interval.P5, Interval.m7], '7sus4');
-  }
-
-  static get ADD_NINTH() {
-    return new ChordType(_symbol, [Interval.P1, Interval.M3, Interval.P5, Interval.M9], 'add9');
-  }
-
-  static get SIXTH() {
-    return new ChordType(_symbol, [Interval.P1, Interval.M3, Interval.P5, Interval.M6], '6');
-  }
-
-  static get AUGMENT_SEVENTH() {
-    return new ChordType(_symbol, [Interval.P1, Interval.M3, Interval.A5, Interval.m7], 'aug7');
-  }
-
-  static get AUGMENT_MAJOR_SEVENTH() {
-    return new ChordType(_symbol, [Interval.P1, Interval.M3, Interval.A5, Interval.M7], 'augM7');
-  }
-
-  static get MINOR_SEVENTH() {
-    return new ChordType(_symbol, [Interval.P1, Interval.m3, Interval.P5, Interval.m7], 'm7');
-  }
-
-  static get MINOR_MAJOR_SEVENTH() {
-    return new ChordType(_symbol, [Interval.P1, Interval.m3, Interval.P5, Interval.M7], 'mM7');
-  }
-
-  static get MINOR_ADD_NINTH() {
-    return new ChordType(_symbol, [Interval.P1, Interval.m3, Interval.P5, Interval.M9], 'madd9');
-  }
-
-  static get MINOR_SIXTH() {
-    return new ChordType(_symbol, [Interval.P1, Interval.m3, Interval.P5, Interval.M6], 'm6');
-  }
-
-  static get HALF_DIMINISH() {
-    return new ChordType(_symbol, [Interval.P1, Interval.m3, Interval.d5, Interval.m7], 'φ');
-  }
-
-  static get DIMINISH_SEVENTH() {
-    return new ChordType(_symbol, [Interval.P1, Interval.m3, Interval.d5, Interval.d7], 'dim7');
-  }
 }
+
+export default {
+  get MAJOR() {
+    return new ChordType([Interval.P1, Interval.M3, Interval.P5], 'M');
+  },
+  get SUSPENDED_FOURTH() {
+    return new ChordType([Interval.P1, Interval.P4, Interval.P5], 'sus4');
+  },
+  get AUGMENT() {
+    return new ChordType([Interval.P1, Interval.M3, Interval.A5], 'aug');
+  },
+  get MINOR() {
+    return new ChordType([Interval.P1, Interval.m3, Interval.P5], 'm');
+  },
+  get DIMINISH() {
+    return new ChordType([Interval.P1, Interval.m3, Interval.d5], 'dim');
+  },
+  get MAJOR_SEVENTH() {
+    return new ChordType([Interval.P1, Interval.M3, Interval.P5, Interval.M7], 'M7');
+  },
+  get SEVENTH() {
+    return new ChordType([Interval.P1, Interval.M3, Interval.P5, Interval.m7], '7');
+  },
+  get SEVENTH_SUSPENDED_FOURTH() {
+    return new ChordType([Interval.P1, Interval.P4, Interval.P5, Interval.m7], '7sus4');
+  },
+  get ADD_NINTH() {
+    return new ChordType([Interval.P1, Interval.M3, Interval.P5, Interval.M9], 'add9');
+  },
+  get SIXTH() {
+    return new ChordType([Interval.P1, Interval.M3, Interval.P5, Interval.M6], '6');
+  },
+  get AUGMENT_SEVENTH() {
+    return new ChordType([Interval.P1, Interval.M3, Interval.A5, Interval.m7], 'aug7');
+  },
+  get AUGMENT_MAJOR_SEVENTH() {
+    return new ChordType([Interval.P1, Interval.M3, Interval.A5, Interval.M7], 'augM7');
+  },
+  get MINOR_SEVENTH() {
+    return new ChordType([Interval.P1, Interval.m3, Interval.P5, Interval.m7], 'm7');
+  },
+  get MINOR_MAJOR_SEVENTH() {
+    return new ChordType([Interval.P1, Interval.m3, Interval.P5, Interval.M7], 'mM7');
+  },
+  get MINOR_ADD_NINTH() {
+    return new ChordType([Interval.P1, Interval.m3, Interval.P5, Interval.M9], 'madd9');
+  },
+  get MINOR_SIXTH() {
+    return new ChordType([Interval.P1, Interval.m3, Interval.P5, Interval.M6], 'm6');
+  },
+  get HALF_DIMINISH() {
+    return new ChordType([Interval.P1, Interval.m3, Interval.d5, Interval.m7], 'φ');
+  },
+  get DIMINISH_SEVENTH() {
+    return new ChordType([Interval.P1, Interval.m3, Interval.d5, Interval.d7], 'dim7');
+  }
+};
